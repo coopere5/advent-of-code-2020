024d75f43fae2410e0f4fc0549b45601b9629a56
Evan Cooper <coopere5@users.noreply.github.com>
2020-12-02 00:21:21 -0500

    finish day 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -18,6 +18,31 @@ namespace Day02
             Stopwatch sw = Stopwatch.StartNew();
 
             var input = File.ReadAllLines("input.txt");
+            var total = 0;
+
+            foreach (var line in input)
+            {
+                var split = line.Split('-', ' ', ':');
+
+                var lower = int.Parse(split[0]);
+                var upper = int.Parse(split[1]);
+                var letter = split[2][0];
+                var password = split[4];
+
+                var count = 0;
+
+                foreach (var c in password)
+                {
+                    if (c == letter)
+                    {
+                        count++;
+                        if (count > upper) continue;
+                    }
+                }
+                if (count >= lower && count <= upper) total++;
+            }
+
+            Console.WriteLine($"Part 1: {total}");
 
             sw.Stop();
             Debug.WriteLine(sw.Elapsed);
@@ -28,6 +53,21 @@ namespace Day02
             Stopwatch sw = Stopwatch.StartNew();
 
             var input = File.ReadAllLines("input.txt");
+            var total = 0;
+
+            foreach (var line in input)
+            {
+                var split = line.Split('-', ' ', ':');
+                
+                var i = int.Parse(split[0]) - 1;
+                var j = int.Parse(split[1]) - 1;
+                var letter = split[2][0];
+                var password = split[4];
+
+                if (password[i] == letter ^ password[j] == letter) total++;
+            }
+
+            Console.WriteLine($"Part 2: {total}");
 
             sw.Stop();
             Debug.WriteLine(sw.Elapsed);
