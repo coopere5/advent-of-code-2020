a013b41392ba10d4080c06fb74f1831354bf64b4
Evan Cooper <coopere5@users.noreply.github.com>
2020-12-01 23:59:56 -0500

    prepare day 2

---
+++ Day02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Day02
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Part1();
+            Part2();
+            Console.ReadLine();
+        }
+
+        private static void Part1()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            var input = File.ReadAllLines("input.txt");
+
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
+        }
+
+        private static void Part2()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            var input = File.ReadAllLines("input.txt");
+
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
+        }
+    }
+}
