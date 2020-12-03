dd0cad131481621f6dd67c496b1530f22816c29d
Evan Cooper <coopere5@users.noreply.github.com>
2020-12-03 00:31:54 -0500

    finish configuring day 4

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace Day04
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Part1();
+            Part2();
+            Console.ReadLine();
+        }
+
+        private static void Part1()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            var input = File.ReadAllLines("input.txt");
+
+            Console.WriteLine($"Part 1: ");
+
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
+        }
+
+        private static void Part2()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            var input = File.ReadAllLines("input.txt");
+
+            Console.WriteLine($"Part 2: ");
+
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
         }
     }
 }
