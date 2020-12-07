e5b6450ec83c7f9472af821fc84120a068885ee4
Evan Cooper <coopere5@users.noreply.github.com>
2020-12-06 22:57:10 -0500

    finish daay 5 with manipulated input

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Day05
 {
@@ -17,9 +19,16 @@ namespace Day05
         {
             Stopwatch sw = Stopwatch.StartNew();
 
-            var input = File.ReadAllLines("input.txt");
+            var input = File.ReadAllLines("manipinput.txt");    //todo: do this without manipulated input
 
-            Console.WriteLine($"Part 1: ");
+            int max = 0;
+            foreach (var line in input)
+            {
+                var split = line.Split(' ');
+                max = Math.Max(max, Convert.ToInt32(split[0], 2) * 8 + Convert.ToInt32(split[1], 2));
+            }
+
+            Console.WriteLine($"Part 1: {max}");
 
             sw.Stop();
             Debug.WriteLine(sw.Elapsed);
@@ -29,7 +38,20 @@ namespace Day05
         {
             Stopwatch sw = Stopwatch.StartNew();
 
-            var input = File.ReadAllLines("input.txt");
+            var input = File.ReadAllLines("manipinput.txt");    //todo: do this without manipulated input
+            var exists = new List<int>();
+
+            int max = 0;
+            foreach (var line in input)
+            {
+                var split = line.Split(' ');
+                var ID = Convert.ToInt32(split[0], 2) * 8 + Convert.ToInt32(split[1], 2);
+                max = Math.Max(max, ID);
+                exists.Add(ID);
+            }
+
+            var all = Enumerable.Range(0, max);
+            var except = all.Except(exists);
 
             Console.WriteLine($"Part 2: ");
 
