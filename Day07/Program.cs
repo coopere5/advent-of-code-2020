04cf77060c95f151d9bd1d2aae69caab25feba89
Evan Cooper <coopere5@users.noreply.github.com>
2020-12-06 23:46:09 -0500

    configure day 7

---
+++ Day07/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day06
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Part1();
+            Part2();
+            Console.ReadLine();
+        }
+
+        private static void Part1()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            var input = File.ReadAllLines("input.txt");
+
+            Console.WriteLine($"Part 1: ");
+
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
+        }
+
+        private static void Part2()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            var input = File.ReadAllLines("input.txt");
+
+            Console.WriteLine($"Part 2: ");
+
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
+        }
+    }
+}
