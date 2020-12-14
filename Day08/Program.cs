e349f8eaeee895651d56ff1a219867dd34d2b27e
Evan Cooper <coopere5@users.noreply.github.com>
2020-12-14 01:52:50 -0500

    finish part 1

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -18,8 +19,29 @@ namespace Day08
 
             var input = File.ReadAllLines("input.txt");
 
+            Dictionary<int, KeyValuePair<string, int>> dict = new Dictionary<int, KeyValuePair<string, int>>();
+            var i = 0;
+            foreach (var line in input)
+            {
+                var split = line.Split(' ');
+                dict.Add(i++, new KeyValuePair<string, int>(split[0], int.Parse(split[1])));
+            }
+
+            var acc = 0;
+            var idx = 0;
+
+            HashSet<int> set = new HashSet<int>();
+            set.Add(idx);
+            var running = true;
+
+            while (running)
+            {
+                ProcessNext(dict, ref acc, ref idx);
+                running = set.Add(idx);
+            }
+
             sw.Stop();
-            Console.WriteLine($"Part 1: ");
+            Console.WriteLine($"Part 1: {acc}");
             Debug.WriteLine(sw.Elapsed);
         }
 
@@ -32,6 +54,23 @@ namespace Day08
             sw.Stop();
             Console.WriteLine($"Part 2: ");
             Debug.WriteLine(sw.Elapsed);
+        }
+
+        private static void ProcessNext(Dictionary<int, KeyValuePair<string, int>> dict, ref int acc, ref int idx)
+        {
+            switch (dict[idx].Key)
+            {
+                case "acc":
+                    acc += dict[idx].Value;
+                    idx++;
+                    break;
+                case "jmp":
+                    idx += dict[idx].Value;
+                    break;
+                case "nop":
+                    idx++;
+                    break;
+            }
         }
     }
 }
